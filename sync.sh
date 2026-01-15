6aaaf6396999c9671d608117e95cce22ed27ac52
Ben Gregory <bengregorygbr@gmail.com>
2026-01-15 22:38:54 +0000

    devops: improved syns.sh script to stop the current container only after the new image is fully built

--- sync.sh
+++ sync.sh
@@ -4,12 +4,14 @@
 # Update project
 git pull --rebase
 
+# Create new image
+sudo docker build -t nuxt-app .
+
+
 # Stop current container
 sudo docker stop nuxt-app
 sudo docker rm nuxt-app
 
-# Create new image 
-sudo docker build -t nuxt-app .
 
 # Start container with new image
 sudo docker run -d --name nuxt-app   -p 3000:3000   --restart=always   nuxt-app
